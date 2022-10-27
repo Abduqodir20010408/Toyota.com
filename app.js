a3a9d6460243e73a22699ed02b6473f9f1b7da47
Abduqodir20010408 <108029303+Abduqodir20010408@users.noreply.github.com>
2022-10-27 10:37:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+const vehicles = document.querySelector(".vehicles")
+const shopping = document.querySelector(".shopping")
+const menu = document.querySelector(".menu")
+
+function showvehicles(){
+  vehicles.classList.toggle("show_vehicles")
+}
+
+function showshopping(){
+  shopping.classList.toggle("show_shopping")
+}
+
+function showmenu(){
+  menu.classList.toggle("show_menu")
+}
